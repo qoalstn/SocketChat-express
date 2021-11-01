27d606ebcd0b67f1ef11c9a8f0ddcbb5c2311449
qoalstn <mousson2020@gmail.com>
2021-11-01 10:19:40 +0900

    create chat

---
+++ socket-chat/socket/public/app.js
@@ -0,0 +1,52 @@
+const socket = io();
+const msgText = document.querySelector('#msg');
+const btnSend = document.querySelector('#btn-send');
+const chatBox = document.querySelector('.chat-content');
+const displayMsg = document.querySelector('.message');
+
+let name;
+do {
+  name = prompt('what is your name?');
+} while (!name);
+
+document.querySelector('#your-name').textContent = name;
+msgText.focus();
+
+btnSend.addEventListener('click', (e) => {
+  e.preventDefault();
+  sendMsg(msgText.value);
+  msgText.value = '';
+  msgText.focus();
+  chatBox.scrollTop = chatBox.scrollHeight;
+});
+
+const sendMsg = (message) => {
+  let msg = {
+    user: name,
+    message: message.trim(),
+  };
+  display(msg, 'you-message');
+
+  socket.emit('sendMessage', msg);
+};
+
+socket.on('sendToAll', (msg) => {
+  //   console.log('msg : ', msg);
+  display(msg, 'other-message');
+  chatBox.scrollTop = chatBox.scrollHeight;
+});
+
+const display = (msg, type) => {
+  const msgDiv = document.createElement('div');
+  let className = type;
+  msgDiv.classList.add(className, 'message-row');
+  let times = new Date().toLocaleDateString();
+
+  let innerText = `
+    <div class="message-title">🙂<span>${msg.user}</span></div>
+    <div class="message-text">${msg.message}</div>
+    <div class="message-time">${times}</div>`;
+
+  msgDiv.innerHTML = innerText;
+  displayMsg.appendChild(msgDiv);
+};
