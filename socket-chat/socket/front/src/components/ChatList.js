f0aa359be2730c71de6080123bddb916797513dd
qoalstn <mousson2020@gmail.com>
2022-01-07 19:02:05 +0900

    fix : input user

--- socket-chat/socket/front/src/components/ChatList.js
+++ socket-chat/socket/front/src/components/ChatList.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const ChatList = () => {
-  return <div>chatList</div>;
+const ChatList = (props) => {
+  const { roomList, getRoomList } = props;
+  console.log(roomList);
+
+  return (
+    <div>
+      <button onClick={getRoomList}>RoomList</button>
+      {roomList.map((room, index) => {
+        return (
+          <div key={index}>
+            {room.id} {room.latest_date}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ChatList;
--- socket-chat/socket/front/src/pages/Main.js
+++ socket-chat/socket/front/src/pages/Main.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 import './Main.css';
 import Chat from '../components/Chat';
+import ChatList from '../components/ChatList';
 
 const baseUrl = 'http://localhost:3001';
 const params = {
@@ -30,29 +31,19 @@ function ChatView() {
   const [type, setType] = useState('');
   const [msgText, setMsgText] = useState('');
   const [msgList, setMsgList] = useState([]);
+  const [roomList, setRoomList] = useState([]);
   // const [socket, setSocket] = useState();
 
   useEffect(() => {
-    setName(prompt('what is your name?'));
-    setRoom(prompt('what is your room?'));
-    setType(prompt('what is your type?'));
-    // async function connect() {
-    //   socket = io.connect('http://localhost:3001', {
-    //     query: `userId=${name}`,
-    //     transports: ['websocket'],
-    //   });
-    // }
-    // connect();
+    // setName(prompt('what is your name?'));
+    // setRoom(prompt('what is your room?'));
+    // setType(prompt('what is your type?'));
   }, []);
 
   // msgText.focus(); // 포커스 설정 시, 여러 윈도우창 중 맨 앞으로 보내진다.
   const clickSend = (e) => {
     e.preventDefault(); // 1. a 태그를 눌러도 href링크로 이동하지 않는다. 2. submit 버튼을 눌러도 새로고침 되지 않는다.(결과는 출력)
 
-    if (msgList.length === 0) {
-      // console.log('조인룸', room);
-      socket.emit('join room', room);
-    }
     sendMsg(msgText);
     setMsgText('');
 
@@ -70,8 +61,9 @@ function ChatView() {
     };
 
     setMsgList([...msgList, msg]);
+    console.log(12121, room);
 
-    let sendData = {
+    const sendData = {
       room_id: room,
       user_type: type,
       username: name,
@@ -88,6 +80,15 @@ function ChatView() {
     socket.emit('conversation', sendData);
   };
 
+  const getRoomList = () => {
+    const data = {
+      user_type: type,
+      user_id: name,
+    };
+    console.log(data);
+    socket.emit('room list', data);
+  };
+
   socket.on('conversation', (message) => {
     console.log('conversation : ', message);
     let msg = {
@@ -98,6 +99,21 @@ function ChatView() {
 
     setMsgList([...msgList, msg]);
     // chatBox.scrollTop = chatBox.scrollHeight;
+  });
+
+  const joinRoom = () => {
+    const data = {
+      user_id: name,
+      mec_id: 1,
+      room_id: room,
+    };
+
+    socket.emit('join room', data);
+  };
+
+  socket.on('room list', (data) => {
+    console.log('roomlist socket_on', data);
+    setRoomList(data);
   });
 
   socket.on('chat message', (data) => {
@@ -114,54 +130,75 @@ function ChatView() {
 
   return (
     <div>
-      <div class="chat-container">
-        <div class="chat-header">
-          <div class="logo">
-            <button onClick={close}>1[ X ]</button>
-            <i class="fa fa-child"></i>
-            <h3>Messenger</h3>
-          </div>
-          <span> Room : </span>
-          <p id="room-name">{room}</p>
-          <span> User : </span>
-          <p id="your-name">{name}</p>
-        </div>
-
-        <div class="chat-section">
-          <div class="main-wrapper">
-            <div class="chat-content">
-              <div class="message">
-                {msgList.map((i, index) => {
-                  return (
-                    <Chat
-                      key={index}
-                      type={i.type}
-                      user={i.user}
-                      message={i.message}
-                      time={i.time}
-                    />
-                  );
-                })}
-              </div>
+      <div style={{ margin: 30 }}>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="userId"
+        />
+        <input
+          type="text"
+          onChange={(e) => setRoom(e.target.value)}
+          placeholder="roomId"
+        />
+        <input
+          type="text"
+          onChange={(e) => setType(e.target.value)}
+          placeholder="userType"
+        />
+        <button onClick={joinRoom}>joinRoom</button>
+      </div>
+      <div className="main-container">
+        <div class="chat-container">
+          <div class="chat-header">
+            <div class="logo">
+              <button onClick={close}>1[ X ]</button>
+              <i class="fa fa-child"></i>
+              <h3>Messenger</h3>
             </div>
-            <form class="msg-tex">
-              <input
-                type="text"
-                name="msg"
-                id="msg"
-                placeholder="Message Here.."
-                autoComplete="off"
-                value={msgText}
-                onChange={(e) => {
-                  setMsgText(e.target.value);
-                }}
-              />
-              <button id="btn-send" onClick={clickSend}>
-                <i class="fa fa-paper-plane">send</i>
-              </button>
-            </form>
+            <span> Room : </span>
+            <p id="room-name">{room}</p>
+            <span> User : </span>
+            <p id="your-name">{name}</p>
+          </div>
+
+          <div class="chat-section">
+            <div class="main-wrapper">
+              <div class="chat-content">
+                <div class="message">
+                  {msgList.map((i, index) => {
+                    return (
+                      <Chat
+                        key={index}
+                        type={i.type}
+                        user={i.user}
+                        message={i.message}
+                        time={i.time}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+              <form class="msg-tex">
+                <input
+                  type="text"
+                  name="msg"
+                  id="msg"
+                  placeholder="Message Here.."
+                  autoComplete="off"
+                  value={msgText}
+                  onChange={(e) => {
+                    setMsgText(e.target.value);
+                  }}
+                />
+                <button id="btn-send" onClick={clickSend}>
+                  <i class="fa fa-paper-plane">send</i>
+                </button>
+              </form>
+            </div>
           </div>
         </div>
+        <ChatList roomList={roomList} getRoomList={getRoomList} />
       </div>
     </div>
   );
